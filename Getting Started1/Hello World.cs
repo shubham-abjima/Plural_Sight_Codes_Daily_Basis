f9e74a02e44534611e9c752c901c4f2f0dab1fba
Shubham Khandelwal <shubham.khandelwal@abjima.com>
2022-07-05 17:49:47 +0530

    learning of 05 july

---
+++ Getting Started1/Hello World.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        // Hello world
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        // fahrenheit to celcius converter
+        static float FahrenheitToCelcius(float temperature)
+        {
+            float tempratureCelsius = (temperature - 32) / 1.8f;
+            return tempratureCelsius;
+        }
+    }
+}
