687ca6453342abacb01e61bda073edf3908cf82c
Shubham Khandelwal <shubham.khandelwal@abjima.com>
2022-07-06 17:30:26 +0530

    learning of 06 July 2022_Updated

--- 06 July_2022/06 July_2022/Hello World.cs
+++ 06 July_2022/06 July_2022/Hello World.cs
@@ -1,15 +1,15 @@
-﻿//using MyUtilities;
+﻿using MyUtilities;
 
-//namespace MyFirstProgram
-//{
-//    class CheckComfort
-//    {
-//        static void Main(string[] args)
-//        {
-//            WeatherUtilities.Report("Maldives", 23, 65);
-//            WeatherUtilities.Report("Rajasthan", WeatherUtilities.FahrenheitToCelcius(25), 65);
-//            WeatherUtilities.Report("*****", WeatherUtilities.FahrenheitToCelcius(32), 64);
-//            WeatherUtilities.Report("Europe", WeatherUtilities.CelciusToFahrenheit(25), 65);
-//        }
-//    }
-//}
+namespace MyFirstProgram
+{
+    class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            WeatherUtilities.Report("Maldives", 23, 65);
+            WeatherUtilities.Report("Rajasthan", WeatherUtilities.FahrenheitToCelcius(25), 65);
+            WeatherUtilities.Report("*****", WeatherUtilities.FahrenheitToCelcius(32), 64);
+            WeatherUtilities.Report("Europe", WeatherUtilities.CelciusToFahrenheit(25), 65);
+        }
+    }
+}
--- 06 July_2022/06 July_2022/WeatherUtlities.cs
+++ 06 July_2022/06 July_2022/WeatherUtlities.cs
@@ -1,33 +1,33 @@
-﻿//using System;
+﻿using System;
 
-//namespace MyUtilities
-//{
-//    class WeatherUtilities
-//    {
-//        public static float FahrenheitToCelcius(float temperatureFahrenheit)
-//        {
-//            return  (temperatureFahrenheit - 32) / 1.8f;
-       
-//        }
-//        public static float CelciusToFahrenheit(float temperatureCelcius)
-//        {
-//            return (temperatureCelcius * 1.8f) + 32;
-  
-//        }
-//        private static float ComfortIndex(float temperatureFahrenheit, float humidity)
-//        {
-//            return (temperatureFahrenheit + humidity) / 4;
-   
+namespace MyUtilities
+{
+    class WeatherUtilities
+    {
+        public static float FahrenheitToCelcius(float temperatureFahrenheit)
+        {
+            return (temperatureFahrenheit - 32) / 1.8f;
 
-//        }
-//        static public void Report(string location, float temperatureCelcius, float humidity)
-//        {
-//            var temperaturefahrenheit = CelciusToFahrenheit(temperatureCelcius);
-//            Console.WriteLine($"Comfort Index for {location}:{ComfortIndex(temperaturefahrenheit, humidity)}");
-//        }
-//        //static void Main(string[] args)
-//        //{
-//        //    WeatherUtilities.Report("Maldives", 23, 65);
+        }
+        public static float CelciusToFahrenheit(float temperatureCelcius)
+        {
+            return (temperatureCelcius * 1.8f) + 32;
 
-//    }
-//}
+        }
+        private static float ComfortIndex(float temperatureFahrenheit, float humidity)
+        {
+            return (temperatureFahrenheit + humidity) / 4;
+
+
+        }
+        static public void Report(string location, float temperatureCelcius, float humidity)
+        {
+            var temperaturefahrenheit = CelciusToFahrenheit(temperatureCelcius);
+            Console.WriteLine($"Comfort Index for {location}:{ComfortIndex(temperaturefahrenheit, humidity)}");
+        }
+        //static void Main(string[] args)
+        //{
+        //    WeatherUtilities.Report("Maldives", 23, 65);
+
+    }
+}
